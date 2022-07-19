4c6e0cd268335d2a5f274424a7afb67ab939e12c
Cleifson Araújo <cl.raujo1@gmail.com>
2022-07-19 16:22:13 -0300

    Update Disciplina.cpp
    blablabla

--- src/Disciplina.cpp
+++ src/Disciplina.cpp
@@ -3,18 +3,18 @@
 #include "algorithm"
 #include <iostream>
 
-Disciplina::Disciplina(std::string codigo, std::string nome) //método construtor para a inicialização dos atributos.
+Disciplina::Disciplina(std::string codigo, std::string nome) // método construtor para a inicialização dos atributos.
 {
     this->codigo = codigo;
     this->nome = nome;
 }
 
-void Disciplina::inserir_alocacao(std::string dia, std::string horario, std::string sala) //cria uma nova alocação para a disciplina a partir dos dados informados e insere na coleção.
+void Disciplina::inserir_alocacao(std::string dia, std::string horario, std::string sala) // cria uma nova alocação para a disciplina a partir dos dados informados e insere na coleção.
 {
     myList.push_front(Alocacao(dia, horario, sala));
 }
 
-void Disciplina::remover_alocacao(std::string horario) //remove uma alocação da coleção. Para facilitar, vamos assumir que existe apenas uma única alocação em cada horário.
+void Disciplina::remover_alocacao(std::string horario) // remove uma alocação da coleção. Para facilitar, vamos assumir que existe apenas uma única alocação em cada horário.
 {
     for (auto iter = myList.begin(); iter != myList.end(); iter++) {
         if ((*iter).horario == horario)
@@ -22,10 +22,11 @@ void Disciplina::remover_alocacao(std::string horario) //remove uma alocação d
     }
 }
 
-void Disciplina::imprimir_alocacao() //imprime todas as alocações para a disciplina. Cada alocação deve ser impressa em uma linha separada e seguindo o formato "codigo nome dia horario sala". Além disso, a alocação deve ser impressa ordenada de acordo com o horário.
+void Disciplina::imprimir_alocacao() // imprime todas as alocações para a disciplina. Cada alocação deve ser impressa em uma linha separada e seguindo o formato "codigo nome dia horario sala". Além disso, a alocação deve ser impressa ordenada de acordo com o horário.
 {
     for (auto iter = myList.begin(); iter != myList.end(); iter++) {
         std::cout << this->codigo << " " << this->nome << " ";
         (*iter).imprimir_dados();
     }
 }
+kkkkk
